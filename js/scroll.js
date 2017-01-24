77752fc07adba200aea016e057fac5c7a3367617
flattern <tosha.salogvinov@yandex.ru>
2017-01-24 14:43:23 +0300

    initial commit

--- js/scroll.js
+++ js/scroll.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     $('a[href^="#s-"]').click(function(){
         //Сохраняем значение атрибута href в переменной:
         var target = $(this).attr('href');
-        $('html, body').animate({scrollTop: $(target).offset().top}, 500);
+        $('html, body').animate({scrollTop: $(target).offset().top}, 800);
         return false;
     });
 
